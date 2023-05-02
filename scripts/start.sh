91f39fa2b7b0d8dec7d693a44417f664c27b92e4
Utkarsh Deep <utkarsh@toothsi.in>
2023-05-01 19:17:05 -0700

    fix port

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ processName=$2
 cd $destDir
 npm i --no-audit
 pm2 delete $processName
-pm2 start --time --name=$processName npm -- start -- -p 4000
+pm2 start --time --name=$processName npm -- start -- -p 2000
